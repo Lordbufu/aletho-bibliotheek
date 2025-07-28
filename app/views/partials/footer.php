e543718572b85df10dce52e03780a3f504d06df6
LordBufu <bufu_39@hotmail.com>
2025-07-28 14:25:36 +0200

    removing files that will be relocated.

--- app/views/partials/footer.php
+++
@@ -1,4 +0,0 @@
-		<footer></footer>
-		<!-- Manual Bootstrap min js: -->
-		<script src="js/bootstrap/bootstrap.bundle.min.js"></script>
-		<!-- PHP code for device specific js/jquery etc goes here -->
