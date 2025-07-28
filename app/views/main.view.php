cb5bbbf05893a84933522cc4dc233e2d353de880
LordBufu <bufu_39@hotmail.com>
2025-07-28 10:25:06 +0200

    Adjusting require links, to match the current directory structure.

--- app/views/main.view.php
+++ app/views/main.view.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-	<?php require 'templates\header.php'; ?>
+	<?php require 'partials\header.php'; ?>
 
 	<!-- /* Body to set the main page style and size. */ -->
 	<body class="container-fluid secundary-color-2 p-1 text-center">
 		<?php													// Build page with requires, based on session based user data.
-			require 'templates\banner.php';						// Always load the banner its is also based on user based session data.
+			require 'partials\banner.php';						// Always load the banner its is also based on user based session data.
 
 			if(!isset($userType)) {								// Check user type,
-				require 'templates\login.container.php';		// load login container if not set;
+				require 'partials\login.container.php';			// load login container if not set;
 			} else {
-				require 'templates\view.container.php';			// load view container if set.
+				require 'partials\view.container.php';			// load view container if set.
 			}
 
-			require 'templates\footer.php';						// Always load the footer its also based on user session data.
+			require 'partials\footer.php';						// Always load the footer its also based on user session data.
 		?>
 	</body>
 </html>
