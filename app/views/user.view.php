a7166d62169814fd59bb16107350e76f395fff2c
LordBufu <bufu_39@hotmail.com>
2025-07-31 14:57:30 +0200

    Cleaning up the main view, to see if i can find a small issue related to the item-container.

--- app/views/user.view.php
+++ app/views/user.view.php
@@ -1,16 +1,14 @@
 <!-- /* View content container, to style/position the main content. */ -->
 <div class="view-container container-fluid" id="view-container">
-
-  <?php require 'partials/containers/search-container.php'; ?>
-  <?php require 'partials/containers/hamburger-container.php'; ?>
-
-  <?php foreach ($books as $book): ?>
-	<?php require 'partials/containers/item-container.php'; ?>
-  <?php endforeach; ?>
+<?php
+    require 'partials/containers/search-container.php';
+    require 'partials/containers/hamburger-container.php';
+    foreach ($books as $book) {
+        require 'partials/containers/item-container.php';
+    }
+    require 'partials/popins/add-book-popin.php';
+    require 'partials/popins/status-period-popin.php';
+    require 'partials/popins/password-reset-popin.php';
+    require 'partials/popins/change-book-status-popin.php';
+?>
 </div>
-
-
-<?php require 'partials/popins/add-book-popin.php'; ?>
-<?php require 'partials/popins/status-period-popin.php'; ?>
-<?php require 'partials/popins/password-reset-popin.php'; ?>
-<?php require 'partials/popins/change-book-status-popin.php'; ?>
