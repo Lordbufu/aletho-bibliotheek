a8c60b1937dcb182eed56906e2dc17037f1c9857
LordBufu <bufu_39@hotmail.com>
2025-07-31 14:53:49 +0200

    Adjusting data for the prototype testing.

--- app/controllers/ProtoTypeController.php
+++ app/controllers/ProtoTypeController.php
@@ -6,54 +6,23 @@ use App\Core\App;
 
 class ProtoTypeController {
     public function home() {
+        $user = [ 'name' => 'user', 'is_loaner' => 1, 'is_office_admin' => 0, 'is_global_admin' => 0 ];
+        // $user = [ 'name' => 'officeAdmin', 'is_loaner' => 0, 'is_office_admin' => 1, 'is_global_admin' => 0, 'office_id' => 1 ];
+        // $user = [ 'name' => 'globaleAdmin', 'is_loaner' => 0, 'is_office_admin' => 0, 'is_global_admin' => 1 ];
+
         $data = [
-            'userType' => 'office_admins', // Can be user, office_admins, or global_admins
-            'currentOfficeId' => 1,
-            //'userType' => 'global_admins', // Can be user, office_admins, or global_admins
-            //'userType' => 'user', // Can be user, office_admins, or global_admins
+            'user' => [ 'name'=> $user['name'] ],
+            'perm' => App::get('perm')->setUser($user),
             'books' => [
-                [   'id' => 1,
-                    'name' => 'Book 1',
-                    'author' => 'Author 1',
-                    'genre' => 'Roman',
-                    'office' => 'Hoofdkantoor',
-                    'office_id' => 1
-                ],
-                [   'id' => 2,
-                    'name' => 'Book 2',
-                    'author' => 'Author 2',
-                    'genre' => 'Non-fictie',
-                    'office' => 'Filiaal Noord',
-                    'office_id' => 2
-                ]
+                [ 'id' => 1, 'name' => 'Book 1', 'author' => 'Author 1', 'genre' => 'Roman', 'office_id' => 1, 'status_id' => 1 ],
+                [ 'id' => 2, 'name' => 'Book 2', 'author' => 'Author 2', 'genre' => 'Non-fictie', 'office_id' => 2, 'status_id' => 3 ],
+                [ 'id' => 2, 'name' => 'Book 3', 'author' => 'Author 1', 'genre' => 'Science Fiction', 'office_id' => 1, 'status_id' => 2 ],
             ],
             'statuses' => [
-                [   'id' => 1,
-                    'type' => 'Status 1',
-                    'periode_length' => 30,
-                    'reminder_day' => 5,
-                    'overdue_day' => 2 ],
-                [   'id' => 2,
-                    'type' => 'Status 2',
-                    'periode_length' => 14,
-                    'reminder_day' => 3,
-                    'overdue_day' => 1 ],
-                [   'id' => 3,
-                    'type' => 'Status 3',
-                    'periode_length' => 60,
-                    'reminder_day' => 10,
-                    'overdue_day' => 5 ]
-            ],
-            'statusTypes' => [
                 [ 'id' => 1, 'type' => 'Uitgeleend' ],
                 [ 'id' => 2, 'type' => 'Beschikbaar' ],
                 [ 'id' => 3, 'type' => 'Gereserveerd' ],
                 [ 'id' => 4, 'type' => 'Verloren' ]
-            ],
-            'users' => [
-                ['id' => 1, 'name' => 'Alice'],
-                ['id' => 2, 'name' => 'Bob'],
-                ['id' => 3, 'name' => 'Charlie']
             ],
             'genres' => [
                 ['id' => 1, 'name' => 'Roman'],
@@ -63,6 +32,11 @@ class ProtoTypeController {
             'offices' => [
                 ['id' => 1, 'name' => 'Hoofdkantoor'],
                 ['id' => 2, 'name' => 'Filiaal Noord']
+            ],
+            'loanerHistory' => [
+                ['id' => 1, 'name' => 'Alice'],
+                ['id' => 2, 'name' => 'Bob'],
+                ['id' => 3, 'name' => 'Charlie']
             ]
         ];
 
