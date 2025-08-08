821f324c81b9b42fcfdac5f21cd0337c67063924
LordBufu <bufu_39@hotmail.com>
2025-08-08 15:32:25 +0200

    Re-factoring the controller data

--- app/controllers/ProtoTypeController.php
+++ app/controllers/ProtoTypeController.php
@@ -3,48 +3,38 @@
 namespace App\Controllers;
 
 use App\Core\App;
-// use DateTime;
 
 class ProtoTypeController {
     public function home() {
-        // $user = [ 'name' => 'user', 'is_loaner' => 1, 'is_office_admin' => 0, 'is_global_admin' => 0 ];
-        // $user = [ 'name' => 'officeAdmin', 'is_loaner' => 0, 'is_office_admin' => 1, 'is_global_admin' => 0, 'office_id' => 1 ];
-        $user = [ 'name' => 'globaleAdmin', 'is_loaner' => 0, 'is_office_admin' => 0, 'is_global_admin' => 1 ];
+        //unset($_SESSION['user']);
+        // Init user test data
+        //App::get('auth')->check();
+        //App::get('auth')->login('test', 'test');
 
         // Testing status expire data display/formating
         $dateObj = new \DateTime('25-01-1873');
-        $statusExp = $dateObj->format('Y-m-d');
+        $bookStatusExp = $dateObj->format('Y-m-d');
+        //$error = "Uw inlog gegevens zijn niet correct, probeer het nogmaals!";
+        $error ='';
+
+
+        $books = [['id' => 1, 'name' => 'Book 1', 'author' => 'Author 1', 'genre' => 'Roman', 'office_id' => 1, 'status_id' => 1], ['id' => 2, 'name' => 'Book 2', 'author' => 'Author 2', 'genre' => 'Non-fictie', 'office_id' => 2, 'status_id' => 3], ['id' => 3, 'name' => 'Book 3', 'author' => 'Author 1', 'genre' => 'Science Fiction', 'office_id' => 1, 'status_id' => 2]];
+        $statuses = [['id' => 1, 'type' => 'Uitgeleend', 'periode_length' => 14, 'reminder_day' => 7, 'overdue_day' => 15], ['id' => 2, 'type' => 'Beschikbaar', 'periode_length' => 38, 'reminder_day' => 30, 'overdue_day' => 39], ['id' => 3, 'type' => 'Gereserveerd', 'periode_length' => 7, 'reminder_day' => 5, 'overdue_day' => 8], ['id' => 4,'type' => 'Verloren', 'periode_length' => 27, 'reminder_day' => 20, 'overdue_day' => 28]];
+        $genres = [['id' => 1, 'name' => 'Roman'], ['id' => 2, 'name' => 'Non-fictie'], ['id' => 3, 'name' => 'Science Fiction']];
+        $offices = [['id' => 1, 'name' => 'Hoofdkantoor'], ['id' => 2, 'name' => 'Filiaal Noord']];
+        $loanerHistory = [['id' => 1, 'name' => 'Alice'], ['id' => 2, 'name' => 'Bob'], ['id' => 3, 'name' => 'Charlie']];
 
         $data = [
-            'user' => [ 'name'=> $user['name'] ],
-            'perm' => App::get('perm')->setUser($user),
-            'statusExp' => $statusExp,
-            'books' => [
-                [ 'id' => 1, 'name' => 'Book 1', 'author' => 'Author 1', 'genre' => 'Roman', 'office_id' => 1, 'status_id' => 1 ],
-                [ 'id' => 2, 'name' => 'Book 2', 'author' => 'Author 2', 'genre' => 'Non-fictie', 'office_id' => 2, 'status_id' => 3 ],
-                [ 'id' => 3, 'name' => 'Book 3', 'author' => 'Author 1', 'genre' => 'Science Fiction', 'office_id' => 1, 'status_id' => 2 ],
-            ],
-            'statuses' => [
-                [ 'id' => 1, 'type' => 'Uitgeleend' ],
-                [ 'id' => 2, 'type' => 'Beschikbaar' ],
-                [ 'id' => 3, 'type' => 'Gereserveerd' ],
-                [ 'id' => 4, 'type' => 'Verloren' ]
-            ],
-            'genres' => [
-                ['id' => 1, 'name' => 'Roman'],
-                ['id' => 2, 'name' => 'Non-fictie'],
-                ['id' => 3, 'name' => 'Science Fiction']
-            ],
-            'offices' => [
-                ['id' => 1, 'name' => 'Hoofdkantoor'],
-                ['id' => 2, 'name' => 'Filiaal Noord']
-            ],
-            'loanerHistory' => [
-                ['id' => 1, 'name' => 'Alice'],
-                ['id' => 2, 'name' => 'Bob'],
-                ['id' => 3, 'name' => 'Charlie']
-            ]
+            'statusExp' => $bookStatusExp,
+            'books' => $books,
+            'statuses' => $statuses,
+            'genres' => $genres,
+            'offices' => $offices,
+            'loanerHistory' => $loanerHistory,
+            'error' => $error
         ];
+
+        //debugData($_SESSION);
 
         return App::view('main.view', $data);
     }
