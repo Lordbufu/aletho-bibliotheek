7d8595130e17e5c7e64b174084bb91a29be5d7a7
LordBufu <bufu_39@hotmail.com>
2025-09-18 16:03:28 +0200

    Cleaning up the app routers, to have more explicit controller function names after the login process.

--- ext/config/routes.php
+++ ext/config/routes.php
@@ -5,16 +5,8 @@ $router->get(       '/login',           'AuthController@showform');
 $router->post(      '/login',           'AuthController@authenticate');
 $router->post(      '/logout',          'AuthController@logout');
 
-/* App view routes */
-$router->get(       '/home',            'ViewController@userLogin');
+/* App main view routes */
+$router->get(       '/home',            'ViewController@home');
 
-/* App update routes */
+/* App interaction routes */
 $router->patch(    '/resetPassword',   'AuthController@resetPassword');
-
-
-/* Router testing: routes */
-    // $router->get('/test/{id}', 'ProtoTypeController@guid');
-    // $router->get('/test/{id:\d+}', 'ProtoTypeController@guid');
-    // $router->get('/quick', function ($request, $response) {
-    //     $response->setContent('Hello world')->send();
-    // });
