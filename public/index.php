de61dcda5888df01e5683ca410b47708624e902b
LordBufu <bufu_39@hotmail.com>
2025-09-18 16:01:55 +0200

    Cleaning up the landing point index.php

--- public/index.php
+++ public/index.php
@@ -3,24 +3,27 @@ require __DIR__ . '/../bootstrap.php';
 
 use App\App;
 
-if (!App::boot()) {
-    // dd(App::getBootErrors());
-    handleBootFailure(App::getBootErrors());
+if (!App::boot()) {                                         // Attempt to boot the App
+    // dd(App::getBootErrors());                               // debug logging option
+    handleBootFailure(App::getBootErrors());                // Log\Displays errors if failed
     exit;
 }
 
-// Start session, and check for a user-agent
-session_start();
-$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? null;
+session_start();                                            // Start default session here
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? null;           // Set user_agent if present or set to null
 
-// If a user agent is set, we consider the user valid, and set a guest tag if no user data is set.
-if ($userAgent) {
-    if (empty($_SESSION['user'])) {
+if ($userAgent) {                                           // Default user-agent check
+    if (empty($_SESSION['user'])) {                         // Set role to 'Guest' for initial login routing
         $_SESSION['user'] = ['role' => 'Guest'];
+    }
+    
+    if (!isset($_SESSION['user']['role'])) {                // Included check for lost\altered session data
+        echo 'no user data found';    
+        exit;
     }
 } else {
     echo 'no user-agent found';
     exit;
 }
 
-App::getService('router')->dispatch();
+App::getService('router')->dispatch();                      // Router requests
