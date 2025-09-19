3436ff6db0a76aa31092f3e3624182ae64edc5e7
LordBufu <bufu_39@hotmail.com>
2025-09-19 11:26:06 +0200

    Adding slightly less development like account names as default data

--- ext/schema/data/01_users.sql
+++ ext/schema/data/01_users.sql
@@ -2,7 +2,7 @@
 INSERT INTO `users`
     (`id`, `name`, `password`, `email`, `is_loaner`, `is_office_admin`, `is_global_admin`, `create_at`, `updated_at`, `active`)
         VALUES
-    (NULL, 'user', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '1', '0', '0', current_timestamp(), current_timestamp(), '1'),
-    (NULL, 'assen_admin', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '1', '0', current_timestamp(), current_timestamp(), '1'),
-    (NULL, 'groningen_admin', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '1', '0', current_timestamp(), current_timestamp(), '1'),
-    (NULL, 'global_admin', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '0', '1', current_timestamp(), current_timestamp(), '1');
+    (NULL, 'Bibliotheek', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '1', '0', '0', current_timestamp(), current_timestamp(), '1'),
+    (NULL, 'AdminAssen', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '1', '0', current_timestamp(), current_timestamp(), '1'),
+    (NULL, 'AdminGro', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '1', '0', current_timestamp(), current_timestamp(), '1'),
+    (NULL, 'Hoofdadmin', '$2y$10$o3P95WU2xrKRfYUye4.DbOaocrJ4.//bf4GQPdjKwirZ4SpK2pvXy', 'test@aletho.nl', '0', '0', '1', current_timestamp(), current_timestamp(), '1');
