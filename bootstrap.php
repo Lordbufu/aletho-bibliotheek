08e304f42be08480d8af41ca4b98d25b83faf0de
LordBufu <bufu_39@hotmail.com>
2025-09-19 09:51:22 +0200

    Updated comments, and added a custom php log file location.

--- bootstrap.php
+++ bootstrap.php
@@ -4,23 +4,37 @@ define('BASE_PATH', realpath(__DIR__));
 require BASE_PATH . '/vendor/autoload.php';
 require BASE_PATH . '/ext/helpers.php';
 
+/**
+ * Set enviroment related config stuff.
+ */
 use Dotenv\Dotenv;
-
 Dotenv::createImmutable(BASE_PATH)->safeLoad();
-
 $env = $_ENV['APP_ENV'] ?? 'production';
 
 /**
- * Session Configuration:
- *  - Custom session cache storage.
- *  - Garbage Collection settings.
- *  - Secure cookies.
+ * Create & set session cache file path, and set garbage collection.
  */
 $sesTempPath = BASE_PATH . '/ext/storage/cache';
-if (!is_dir($sesTempPath)) { mkdir($sesTempPath, 0700, true); }
+if (!is_dir($sesTempPath)) {
+    mkdir($sesTempPath, 0700, true);
+}
 session_save_path($sesTempPath);
 ini_set('session.gc_maxlifetime', 86400);
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 100);
+
+/**
+ * Turn on, create & set custom php error log location.
+ */
+$logTempPath = BASE_PATH . '/ext/storage/php-log';
+if (!is_dir($logTempPath)) {
+    mkdir($logTempPath, 0700, true);
+}
+ini_set('log_errors', 'On');
+ini_set('error_log', '/path/to/your/custom_error.log');
+
+/**
+ * Set secure session cookies.
+ */
 ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_httponly', 1);
