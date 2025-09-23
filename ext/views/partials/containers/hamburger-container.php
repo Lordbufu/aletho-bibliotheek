0056ebfb7bdd46301018c62091e8482df7a1abe1
LordBufu <bufu_39@hotmail.com>
2025-09-23 15:35:46 +0200

    Minor formating change, dropdown is JQuery related.

--- ext/views/partials/containers/hamburger-container.php
+++ ext/views/partials/containers/hamburger-container.php
@@ -1,17 +1,12 @@
-<div class="aletho-dropdown-body collapse" id="customHamburgerDropdown">
+<div class="aletho-dropdown-body collapse" id="customHamburgerDropdown" data-toggle-target="#id">
     <div class="hamburger-container" id="hamb-cont">
-
         <?php if(isset($_SESSION['user']) && $_SESSION['user']['canEdit']): ?>
             <input class="aletho-buttons" id="boek-add-button" type="submit" value="Boek Toevoegen">
             <input class="aletho-buttons" id="status-periode-button" type="submit" value="Periode Wijzigen">
             <input class="aletho-buttons" id="password-change-button" type="submit" value="Wachtwoord Wijzigen">
         <?php endif; ?>
-
         <form action="/logout" method="post">
-            <button type="submit" class="aletho-buttons" id="logoff-button">
-                Uitloggen
-            </button>
+            <button type="submit" class="aletho-buttons" id="logoff-button">Uitloggen</button>
         </form>
-            
     </div>
 </div>
--- ext/views/partials/templates/banner.php
+++ ext/views/partials/templates/banner.php
@@ -1,17 +1,24 @@
 <nav class="aletho-header banner-nav">
 	<?php if( $_SESSION['user']['role'] === 'guest' ) : ?>
-	<img class="banner-image" alt="Alétho Logo" src="/images/huisstijl/Logo-bibliotheek-Wit-RGB.png">
-	<?php else : ?>
-    <button id="hamburger-button" class="aletho-menu-buttons fas fa-bars" type="button"
-    	data-bs-toggle="collapse" data-bs-target="#customHamburgerDropdown"
-    	aria-expanded="false" aria-controls="customHamburgerDropdown"
-	></button>
-		
-	<img class="banner-image" alt="Alétho Logo" src="/images/huisstijl/Logo-bibliotheek-Wit-RGB.png">
-
-    <button id="search-button" class="aletho-menu-buttons" type="button"
-    	data-bs-toggle="collapse" data-bs-target="#customSearchDropdown"
-    	aria-expanded="false" aria-controls="customSearchDropdown"
-	>&#128269;</button>
+		<img class="banner-image" alt="Alétho Logo" src="/images/huisstijl/Logo-bibliotheek-Wit-RGB.png">
+		<?php else : ?>
+		<button id="hamburger-button"
+			class="aletho-menu-buttons fas fa-bars"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#customHamburgerDropdown"
+			aria-expanded="false"
+			aria-controls="customHamburgerDropdown">
+		</button>
+			
+		<img class="banner-image" alt="Alétho Logo" src="/images/huisstijl/Logo-bibliotheek-Wit-RGB.png">
+		<button id="search-button"
+			class="aletho-menu-buttons"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#customSearchDropdown"
+			aria-expanded="false"
+			aria-controls="customSearchDropdown">&#128269;
+		</button>
 	<?php endif; ?>
 </nav>
