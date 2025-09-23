f2b559786efd235ea66f838ed0c4caeb59766429
LordBufu <bufu_39@hotmail.com>
2025-09-23 15:27:41 +0200

    Relocating sorting ui to a container template.

--- ext/views/auth/user.view.php
+++ ext/views/auth/user.view.php
@@ -3,21 +3,8 @@
         <?php
             require viewPath('partials/containers/search-container.php');
             require viewPath('partials/containers/hamburger-container.php');
+            require viewPath('partials/containers/sort-container.php');
         ?>
-
-        <div class="sort-container">
-            <div class="sort-group">
-                <label for="sort-options">Sorteren op:</label>
-                <select id="sort-options" class="aletho-select">
-                    <option value="title-asc">Titel oplopend</option>
-                    <option value="title-desc">Titel aflopend</option>
-                    <option value="writer-asc">Schrijver oplopend</option>
-                    <option value="writer-desc">Schrijver aflopend</option>
-                    <option value="genre-asc">Genre oplopend</option>
-                    <option value="genre-desc">Genre aflopend</option>
-                </select>
-            </div>
-        </div>
 
         <div class="items-list">
             <?php foreach ($books as $book) {
