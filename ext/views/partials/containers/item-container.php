593889b13baf68b7ca4045674f6bfb7acd89fe20
LordBufu <bufu_39@hotmail.com>
2025-10-07 14:30:44 +0200

    Adding inline error containers for later.

--- ext/views/partials/containers/item-container.php
+++ ext/views/partials/containers/item-container.php
@@ -13,14 +13,14 @@
         <span class="status-dot statusOne d-flex justify-content-end" id="status-dot-<?= $book['id'] ?>"></span>
     </div>
 
-    <div class="collapse aletho-item-dropdown" id="customItemDropdown-<?= $book['id'] ?>">
+    <div id="customItemDropdown-<?= $book['id'] ?>"
+        class="collapse aletho-item-dropdown <?= (isset($old['book_id']) && $old['book_id'] === $book['id']) ? ' show' : '' ?>">
         <form class="book-edit-form p-1" data-book-id="<?= $book['id'] ?>" method="post" action="/editBook">
 
-            <?php if ($_SESSION['user']['canEdit']): ?>
+            <?php if ($_SESSION['user']['canEdit']): // Title section ?>
                 <input type="hidden" name="_method" value="PATCH">
                 <input type="hidden" name="book_id" value="<?= $book['id'] ?>">
 
-                <!-- Book Name for editing -->
                 <div class="input-group input-group-sm">
                     <input type="text"
                         class="aletho-inputs extra-input-style"
@@ -34,15 +34,15 @@
                         aria-label="Edit Book Name">✏️
                     </button>
                 </div>
+                <?php if (!empty($errors['book_title'])): ?>
+                    <div class="aletho-alert-inline"><?= htmlspecialchars($errors['book_title']) ?></div>
+                <?php endif; ?>
             <?php endif; ?>
 
-            <?php if ($_SESSION['user']['canEdit']): ?>
-                <!-- Writer tags container, to allow multiple-entries, populated by JQuery events. -->
+            <?php if ($_SESSION['user']['canEdit']): // Writers section ?>
                 <div class="writer-tags-container" data-book-id="<?= $book['id'] ?>"></div>
 
-                <!-- Writers Input for editing -->
                 <div class="input-group input-group-sm">
-                    <!-- Writers input as a taggable field -->
                     <input type="text"
                         class="aletho-inputs extra-input-style writer-input"
                         id="book-writer-<?= $book['id'] ?>"
@@ -56,16 +56,16 @@
                             data-swap-targets="#book-writer-<?= $book['id'] ?>"
                             aria-label="Edit Writer">✏️</button>
                 </div>
+                <?php if (!empty($errors['book_writers'])): ?>
+                    <div class="aletho-alert-inline"><?= htmlspecialchars($errors['book_writers']) ?></div>
+                <?php endif; ?>
             <?php else: ?>
-                <!-- Writer name for viewing -->
                 <input type="text" class="aletho-inputs extra-input-style" value="<?= htmlspecialchars($book['writers'] ?? '') ?>" disabled>
             <?php endif; ?>
 
-            <?php if ($_SESSION['user']['canEdit']): ?>
-                <!-- Genre tags container, to allow multiple-entries, populated by JQuery events. -->
+            <?php if ($_SESSION['user']['canEdit']): // Genre section ?>
                 <div class="genre-tags-container" data-book-id="<?= $book['id'] ?>"></div>
 
-                <!-- Genre Name for editing -->
                 <div class="input-group input-group-sm">
                     <input type="text"
                         class="aletho-inputs extra-input-style genre-input"
@@ -80,16 +80,16 @@
                         data-swap-targets="#book-genre-<?= $book['id'] ?>"
                         aria-label="Edit Genre">✏️</button>
                 </div>
+                <?php if (!empty($errors['book_genres'])): ?>
+                    <div class="aletho-alert-inline"><?= htmlspecialchars($errors['book_genres']) ?></div>
+                <?php endif; ?>
             <?php else: ?>
-                <!-- Genre Name for viewing -->
                 <input type="text" class="aletho-inputs extra-input-style" value="<?= htmlspecialchars($book['genres'] ?? 'Onbekend') ?>" disabled>
             <?php endif; ?>
 
-            <?php if ($_SESSION['user']['canEdit'] && $book['canEditOffice']): ?>
-                <!-- Office tags container, to allow multiple-entries, populated by JQuery events. -->
+            <?php if ($_SESSION['user']['canEdit'] && $book['canEditOffice']): // Office section ?>
                 <div class="office-tags-container" data-book-id="<?= $book['id'] ?>"></div>
 
-                <!-- Office Name for editing -->
                 <div class="input-group input-group-sm">
                     <input type="text"
                         class="aletho-inputs extra-input-style office-input"
@@ -104,13 +104,14 @@
                         data-swap-targets="#book-office-<?= $book['id'] ?>"
                         aria-label="Edit Office">✏️</button>
                 </div>
+                <?php if (!empty($errors['book_offices'])): ?>
+                    <div class="aletho-alert-inline"><?= htmlspecialchars($errors['book_offices']) ?></div>
+                <?php endif; ?>
             <?php else: ?>
-                <!-- Office Name for viewing -->
                 <input type="text" class="aletho-inputs extra-input-style" value="<?=htmlspecialchars($book['office']) ?>" disabled>
             <?php endif; ?>
 
-            <?php if ($_SESSION['user']['canEdit']): ?>
-                <!-- Book Status for editing -->
+            <?php if ($_SESSION['user']['canEdit']): // Huidige status section ?>
                 <div class="input-group input-group-sm">
                     <span class="aletho-labels extra-popin-style">Status</span>
                     <div type="button" class="extra-fake-button"></div>
@@ -119,14 +120,15 @@
                     <input type="text" class="aletho-inputs extra-input-style" id="book-status-<?= $book['id'] ?>" name="book_status" value="<?= htmlspecialchars($book['status']['status_name']) ?>" disabled>
                     <div type="button" class="extra-fake-button"></div>
                 </div>
+                <?php if (!empty($errors['book_status'])): ?>
+                    <div class="aletho-alert-inline"><?= htmlspecialchars($errors['book_status']) ?></div>
+                <?php endif; ?>
             <?php else : ?>
-                <!-- Book Status for viewing -->
                 <span class="aletho-labels extra-popin-style">Status</span>
                 <input type="text" class="aletho-inputs extra-input-style" id="book-status-<?= $book['id'] ?>" name="book_status" value="<?= htmlspecialchars($book['status']['status_name']) ?>" disabled>
             <?php endif; ?>
 
-            <?php if (isset($book['status']['status_exp']) && $_SESSION['user']['canEdit']) :?>
-                <!-- Book Status Verloopt Info (voor admins) -->
+            <?php if (isset($book['status']['status_exp']) && $_SESSION['user']['canEdit']): // Status expires section ?>
                 <div class="input-group input-group-sm">
                     <span class="aletho-labels extra-popin-style">Verloopt</span>
                     <div type="button" class="extra-fake-button"></div>
@@ -136,32 +138,27 @@
                     <div type="button" class="extra-fake-button"></div>
                 </div>
             <?php elseif (isset($book['status']['status_exp'])) : ?>
-                <!-- Book Status Verloopt Info -->
                 <span class="aletho-labels extra-popin-style">Verloopt</span>
                 <input type="date" class="aletho-inputs extra-input-style" id="book-status-expires-<?= $book['id'] ?>" name="book_status_expires" value="<?= htmlspecialchars($book['status']['status_exp']) ? htmlspecialchars($book['status']['status_exp']) : '' ?>" disabled>
             <?php endif; ?>
 
-            <?php if($_SESSION['user']['canEdit']): ?>
-                <!-- Previous Loaners Section (W.I.P.) -->
+            <?php if($_SESSION['user']['canEdit']): // Previous loaners section ?>
                 <div class="input-group input-group-sm">
                     <span class="aletho-labels extra-popin-style">Vorige Leners</span>
                     <div type="button" class="extra-fake-button"></div>
                 </div>
                 <div class="input-group input-group-sm">
-                    <!-- Need to refactor this, based on the new books array-->
                     <div type="button" class="extra-fake-button"></div>
                 </div>
             <?php endif; ?>
 
-            <?php if($_SESSION['user']['canEdit']): ?>
-                <!-- Save Book Edits button (form submit) -->
+            <?php if($_SESSION['user']['canEdit']): // Form buttons section ?>
                 <div class="input-group input-group-sm mt-1">
                     <button id="save-changes-<?= $book['id'] ?>"
                         type="submit"
                         class="aletho-buttons extra-popin-style">Wijzigingen Opslaan</button>
                     <div type="button" class="extra-fake-button"></div>
                 </div>
-                <!-- Status Edit pop-in button -->
                 <div class="input-group input-group-sm mt-1">
                     <button id="boek-status-button"
                         type="button"
@@ -169,7 +166,6 @@
                         class="aletho-buttons extra-popin-style">Status Aanpassen</button>
                     <div type="button" class="extra-fake-button"></div>
                 </div>
-                <!-- Delete Book button (form submmit ?) -->
                 <div class="input-group input-group-sm mt-1">
                     <button id="remove-book-<?= $book['id'] ?>"
                         type="submit"
