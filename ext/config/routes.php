2a99421824f1c36a39e72a7b57749bdaf0b0db80
LordBufu <bufu_39@hotmail.com>
2025-10-10 13:49:55 +0200

    Added the book delete route for testing.

--- ext/config/routes.php
+++ ext/config/routes.php
@@ -1,15 +1,16 @@
 <?php
 /* User-Managment routes */
-$router->get(   '/',                'ViewController@landing');
-$router->get(   '/login',           'AuthController@showform');
-$router->post(  '/login',           'AuthController@authenticate');
-$router->post(  '/logout',          'AuthController@logout');
-$router->patch( '/resetPassword',   'AuthController@resetPassword');
+$router->get(       '/',                'ViewController@landing');
+$router->get(       '/login',           'AuthController@showform');
+$router->post(      '/login',           'AuthController@authenticate');
+$router->post(      '/logout',          'AuthController@logout');
+$router->patch(     '/resetPassword',   'AuthController@resetPassword');
 
 /* App main view routes */
-$router->get(   '/home',            'ViewController@home');
+$router->get(       '/home',            'ViewController@home');
 
 /* App book related routes */
-$router->get(   '/bookdata',        'BookController@bookdata');
-$router->post(  '/addBook',         'BookController@add');
-$router->patch( '/editBook',        'BookController@edit');
+$router->get(       '/bookdata',        'BookController@bookdata');
+$router->post(      '/addBook',         'BookController@add');
+$router->patch(     '/editBook',        'BookController@edit');
+$router->delete(    '/delBook',         'BookController@delete');
