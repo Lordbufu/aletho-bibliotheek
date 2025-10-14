0a59d5cba2f0c1c448f29f855b8bb09bb983a1e8
LordBufu <bufu_39@hotmail.com>
2025-10-14 11:39:22 +0200

    Fleshing out the popin partial, for the input an suggestion handling, similar to how edits work.

--- ext/views/partials/popins/add-book-popin.php
+++ ext/views/partials/popins/add-book-popin.php
@@ -1,4 +1,4 @@
-<div id="add-book-popin" class="modal" tabindex="-1" style="display:none;">
+<div id="add-book-popin" class="modal no-backdrop-close clear-on-close" tabindex="-1" style="display:none;">
     <div class="modal-dialog modal-dialog-centered" style="margin:auto;">
         <div class="modal-content aletho-modal-content">
 
@@ -14,31 +14,34 @@
                         class="aletho-inputs extra-input-style"
                         id="book-name-add"
                         name="book_name"
-                        placeholder="Type een boek naam en druk op Enter.">
+                        placeholder="Type een boek naam.">
 
-                    <div class="add-writer-tags-container"></div>
-                    <label for="writer" class="aletho-labels extra-popin-style">Schrijver</label>
+                    <div class="add-writer-tags-container" data-context="popin"></div>
+                    <label for="writer" class="aletho-labels extra-popin-style" data-context="popin">Schrijver</label>
                     <input type="text"
-                        class="aletho-inputs extra-input-style writer-input"
+                        class="aletho-inputs extra-input-style writer-input-pop"
                         id="book-writer-add"
                         placeholder="Type een schrijver naam, en druk op Enter"
-                        autocomplete="off">
+                        autocomplete="off"
+                        data-context="popin">
 
-                    <div class="add-genre-tags-container"></div>
-                    <label for="genre" class="aletho-labels extra-popin-style">Genre</label>
+                    <div class="add-genre-tags-container" data-context="popin"></div>
+                    <label for="genre" class="aletho-labels extra-popin-style" data-context="popin">Genre</label>
                     <input type="text"
-                        class="aletho-inputs extra-popin-style genre-input"
+                        class="aletho-inputs extra-popin-style genre-input-pop"
                         id="book-genre-add"
                         placeholder="Type een genre naam, en druk op Enter"
-                        autocomplete="off">
+                        autocomplete="off"
+                        data-context="popin">
 
-                    <div class="add-office-tags-container"></div>
-                    <label for="office" class="aletho-labels extra-popin-style">Locatie</label>
+                    <div class="add-office-tags-container" data-context="popin"></div>
+                    <label for="office" class="aletho-labels extra-popin-style" data-context="popin">Locatie</label>
                     <input type="text"
-                        class="aletho-inputs extra-popin-style office-input"
+                        class="aletho-inputs extra-popin-style office-input-pop"
                         id="book-office-add"
                         placeholder="Type een locatie naam, en druk op Enter"
-                        autocomplete="off">
+                        autocomplete="off"
+                        data-context="popin">
 
                     <button type="submit" class="aletho-buttons extra-popin-style">Opslaan</button>
                 </form>
