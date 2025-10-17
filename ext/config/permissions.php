c77fa49d74d2e231f0d426eea097b77732723325
LordBufu <bufu_39@hotmail.com>
2025-10-17 09:52:47 +0200

    Forgot to add the change to permission strings, to make authentication code a bit easier imo.

--- ext/config/permissions.php
+++ ext/config/permissions.php
@@ -1,20 +1,20 @@
 <?php
 return [
-    'Global Admin' => [
-        'view_catalog',
-        'manage_books',
-        'manage_offices',
-        'manage_account',
-        'receive_notifications'
-    ], 'Office Admin' => [
-        'view_catalog',
-        'manage_books',
-        'manage_office',
-        'manage_account',
-        'receive_notifications'
+    'Gadmin' => [
+        'viewCata',
+        'manageBooks',
+        'manageOffice',
+        'pwAllChange',
+        'notifications'
+    ], 'Oadmin' => [
+        'viewCata',
+        'manageBooks',
+        'manageOwnOffice',
+        'pwChange',
+        'notifications'
     ], 'User' => [
-        'view_catalog',
-        'receive_notifications'
+        'viewCata',
+        'notifications'
     ], 'Guest' => [
-        'view_login'
+        'viewLogin'
 ]];
