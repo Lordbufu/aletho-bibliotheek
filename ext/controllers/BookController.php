0a0f818e7b11275da0cfab2e6e21466baba436b5
LordBufu <bufu_39@hotmail.com>
2025-10-17 10:21:49 +0200

    Adding the user feedback changes to the book controller, reworking/adding some of the validation/sanitation flow.

--- ext/controllers/BookController.php
+++ ext/controllers/BookController.php
@@ -1,35 +1,38 @@
 <?php
-
-/*  Dealing with errrors and user feedback:
- *      $_SESSION['_flash'] = [
- *          'type'      => 'failure'|'success', 
- *          'message'   => '...'
- *      ]
+/** TODO List:
+ *      - Review the add/edit authentication logic, i might want to change that to just checking there admin status.
+ *      - Review the frontend JQuery for edit and add logic, there seems to be a few key issues atm:
+ *          - Fields not being tagged as editable.
+ *          - Input remains when converted to a Tag.
+ *          - Requires more live testing.
+ *      - Add a default status when adding books, seems logical to just set to avaible right ?
  */
 
 namespace Ext\Controllers;
 
 use App\{App, BooksService, ValidationService};
 
-/** Handles books related user logic. */
+/*  Handles books related user logic. */
 class BookController {
     protected BooksService $bookS;
     protected ValidationService $valS;
 
-    /** Construct BooksService as default local service. */
+    /*  Construct BooksService as default local service. */
     public function __construct() {
-        $this->bookS = new BooksService;
-        $this->valS  = new ValidationService();
-
-        App::getService('logger')->info(
-            "Controller 'BookController' has constructed 'BooksService' and 'ValidationService'",
-            'bookcontroller'
-        );
+        try {
+            $this->bookS = new BooksService;
+            $this->valS  = new ValidationService();
+        } catch(Exception $e) {
+            App::getService('logger')->error(
+                "Failed to construct `BookService` or `ValidationService`",
+                "BookController"
+            );
+            
+            error_log($e.getMessage(), 0);
+        }
     }
 
-    /** Get and return all potentialy known book writers/genres/offices, for form autocomplete features.
-     *      @return void Outputs JSON and exits.
-     */
+    /*  Get and return all potentialy known book writers/genres/offices, for form autocomplete features. */
     public function bookdata() {
         $type = $_GET['data'] ?? '';
 
@@ -48,12 +51,43 @@ class BookController {
         exit;
     }
 
-    /** Filter and process book add form data, and call the add functions in the BooksService.
-     *      @return void Redirects to the landing page route, so user lands on the default view again.
-     */
+    // TODO: Re-add old form data to session _flash ?
+    /*  Filter and process book add form data, and call the add functions in the BooksService. */
     public function add() {
+        $hasError   = false;
+        $newData    = [];
+        $result     = false;
+
         // dd($_POST);
-        $this->bookS->addBook($_POST);
+
+        // Filter out unauthorized users first.
+        if (!App::getService('auth')->can('manageBooks')) {
+            setFlash('global', 'failure', 'Je hebt geen rechten om deze actie uit te voeren.');
+            return App::redirect('/');
+        }
+
+        // Check if POST data exists and is 'sanitized'.
+        if (empty($_POST) || !$this->valS->sanitizeInput($_POST, 'add')) {
+            setFlash('failure', $this->valS->valErrors());
+        } else {
+            $newData = $this->valS->sanData();
+        }
+
+        //  Validate input, and return errors and old input on failure.
+        if (!isset($_SESSION['_flash']['type']) && !$this->valS->validateBookForm($newData, 'add')) {
+            setFlash('failure', $this->valS->valErrors());
+        }
+
+        // Attempt to update book data if no errosr where set.
+        if (!isset($_SESSION['_flash']['type'])) {
+            $result = $this->bookS->addBook($newData);
+            
+            if (!$result) {
+                setFlash('failure', 'Boekgegevens zijn niet toegevoegd.');
+            } else {
+                setFlash('success', 'Boekgegevens zijn toegevoegd.');
+            }
+        }
 
         return App::redirect('/');
     }
@@ -66,51 +100,29 @@ class BookController {
         $newData    = [];
         $result     = false;
 
-        // Filter out unauthorized users first.
-        if (!App::getService('auth')->canUpdateInfo()) {
-            $hasError = setFlash([
-                'type'      => 'failure',
-                'message'   => 'Je hebt geen rechten om deze actie uit te voeren.'
-            ]);
+        if (!App::getService('auth')->can('manageBooks')) {
+            setFlash('failure', 'Je hebt geen rechten om deze actie uit te voeren.');
             return App::redirect('/');
         }
 
-        // Check if POST data exists and is 'sanitized'.
-        if (empty($_POST) || !$this->valS->sanitizeInput($_POST)) {
+        if (empty($_POST) || !$this->valS->sanitizeInput($_POST, 'edit')) {
             $bookId = isset($_POST['book_id']) && is_numeric($_POST['book_id']) ? (int)$_POST['book_id'] : 0;
-            $hasError = setFlash([
-                'type'      => 'failure',
-                'message'   => $this->valS->valErrors(),
-                'old'       => ['book_id' => $bookId]
-            ]);
+            setFlash('failure', $this->valS->valErrors());
         } else {
             $newData = $this->valS->sanData();
         }
 
-        //  Validate input, and return errors and old input on failure.
-        if (!$hasError && !$this->valS->validateBookForm($newData, 'edit')) {
-            $hasError = setFlash([
-                'type'      => 'failure',
-                'message'   => $this->valS->valErrors(),
-                'old'       => $newData
-            ]);
+        if (!isset($_SESSION['_flash']['type']) && !$this->valS->validateBookForm($newData, 'edit')) {
+            setFlash('failure', $this->valS->valErrors());
         }
 
-        // Attempt to update book data if no errosr where set.
-        if (!$hasError) {
+        if (!isset($_SESSION['_flash']['type'])) {
             $result = $this->bookS->updateBook($newData);
             
             if (!$result) {
-                $hasError = setFlash([
-                    'type'      => 'failure',
-                    'message'   => 'Boekgegevens zijn niet bijgewerkt.',
-                    'old'       => $newData
-                ]);
+                setFlash('failure', 'Boekgegevens zijn niet bijgewerkt.');
             } else {
-                $hasError = setFlash([
-                    'type'      => 'success',
-                    'message'   => 'Boekgegevens zijn bijgewerkt.'
-                ]);
+                setFlash('success', 'Boekgegevens zijn bijgewerkt.');
             }
         }
 
