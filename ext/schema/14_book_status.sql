837248c2fcff015f8f7a15fb286f97587780f3ae
LordBufu <bufu_39@hotmail.com>
2025-10-20 15:36:53 +0200

    Preparing for a fleshing out the status library and book related view code.

--- ext/schema/14_book_status.sql
+++ ext/schema/14_book_status.sql
@@ -1,21 +1,24 @@
--- Database Schema for linking books <-> status
+-- Database Schema for linking books <-> status (defaulting to 'aanwezig')
 CREATE TABLE `book_status`(
-    `book_id` INT(11) UNSIGNED NOT NULL,
-    `stat_id` INT(11) UNSIGNED NOT NULL,
-    `meta_id` INT(11) UNSIGNED NOT NULL,
-    `loaner_id` INT(11) UNSIGNED NOT NULL,
-    `current_location` INT(11) UNSIGNED NOT NULL,
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `book_id` INT UNSIGNED NOT NULL,
+    `stat_id` INT UNSIGNED NOT NULL,
+    `meta_id` INT UNSIGNED NULL DEFAULT NULL,
+    `loaner_id` INT UNSIGNED NULL DEFAULT NULL,
+    `current_location` INT(11) UNSIGNED NULL DEFAULT NULL,
     `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `send_mail` BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY(
-        `book_id`,
-        `stat_id`,
-        `meta_id`,
-        `loaner_id`
-    ),
-    FOREIGN KEY(`book_id`) REFERENCES `books`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(`stat_id`) REFERENCES `status`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(`meta_id`) REFERENCES `book_sta_meta`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(`loaner_id`) REFERENCES `loaners`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(`current_location`) REFERENCES `offices`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    `send_mail` TINYINT(1) NOT NULL DEFAULT FALSE,
+    `active` TINYINT(1) NOT NULL DEFAULT 1,
+
+    KEY (`book_id`),
+    KEY (`stat_id`),
+    KEY (`meta_id`),
+    KEY (`loaner_id`),
+    KEY (`current_location`),
+
+  CONSTRAINT `fk_book` FOREIGN KEY (`book_id`) REFERENCES books(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_status` FOREIGN KEY (`stat_id`) REFERENCES status(`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_meta` FOREIGN KEY (`meta_id`) REFERENCES book_sta_meta(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_loaner` FOREIGN KEY (`loaner_id`) REFERENCES loaners(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_location` FOREIGN KEY (`current_location`) REFERENCES offices(`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
--- ext/schema/data/15_global_test_data.sql
+++ ext/schema/data/15_global_test_data.sql
@@ -6,18 +6,37 @@ INSERT INTO `books` (`id`, `title`, `office_id`, `active`) VALUES
     (NULL, 'test book 004', '2', '1'),
     (NULL, 'test book 005', '1', '1'),
     (NULL, 'test book 006', '2', '1');
+
+-- Test writers
+INSERT INTO `writers` (`id`, `name`) VALUES
+    (NULL, 'Test Writer 001'),
+    (NULL, 'Test Writer 002'),
+    (NULL, 'Test Writer 003');
+
 -- Test loaners
 INSERT INTO `loaners` (`id`, `name`, `email`, `office_id`, `active`) VALUES
     (NULL, 'test loaner 001', 'test.loaner001.mail.com', '1', '1'),
     (NULL, 'test loaner 002', 'test.loaner002.mail.com', '2', '1');
--- Test book-status
-INSERT INTO `book_status` (`book_id`, `stat_id`, `meta_id`, `loaner_id`, `current_location`, `start_date`, `send_mail`) VALUES
-    ('1', '1', '1', '1', '1', current_timestamp(), '0'),
-    ('2', '2', '2', '2', '2', current_timestamp(), '0'),
-    ('3', '3', '3', '1', '1', current_timestamp(), '0'),
-    ('4', '4', '4', '2', '2', current_timestamp(), '0'),
-    ('5', '5', '5', '1', '1', current_timestamp(), '0'),
-    ('6', '4', '6', '2', '2', current_timestamp(), '0');
+
+-- Test book-genres
+INSERT INTO `book_genre` (`book_id`, `genre_id`) VALUES
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6');
+
+-- Test book-writers
+INSERT INTO `book_writers` (`book_id`, `writer_id`) VALUES
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '2'),
+    ('5', '1'),
+    ('6', '3'),
+    ('1', '3');
+
 -- Test book-status-meta
 INSERT INTO `book_sta_meta` (`id`, `noti_id`, `action_type`, `action_token`, `token_expires`, `token_used`, `finished`) VALUES
     (NULL, '1', 'Bevestiging01', '123456', '2027-09-01 11:01:36', '0', '1'),
@@ -26,9 +45,12 @@ INSERT INTO `book_sta_meta` (`id`, `noti_id`, `action_type`, `action_token`, `to
     (NULL, '1', 'Bevestiging04', '123459', '2027-09-01 11:01:36', '0', '1'),
     (NULL, '2', 'Herinering05', '123450', '2027-09-01 11:01:36', '0', '1'),
     (NULL, '3', 'Transport verzoek06', '123451', '2027-09-01 11:01:36', '0', '1');
--- Test book-genres
-INSERT INTO `book_genre` (`book_id`, `genre_id`) VALUES ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6');
--- Test writers
-INSERT INTO `writers` (`id`, `name`) VALUES (NULL, 'Test Writer 001'), (NULL, 'Test Writer 002'), (NULL, 'Test Writer 003');
--- Test book-writers
-INSERT INTO `book_writers` (`book_id`, `writer_id`) VALUES ('1', '1'), ('2', '2'), ('3', '3'), ('4', '2'), ('5', '1'), ('6', '3'), ('1', '3');
+
+-- Test book-status
+INSERT INTO `book_status` (`book_id`, `stat_id`, `meta_id`, `loaner_id`, `current_location`, `start_date`, `send_mail`) VALUES
+    ('1', '1', '1', '1', '1', current_timestamp(), '0'),
+    ('2', '2', '2', '2', '2', current_timestamp(), '0'),
+    ('3', '3', '3', '1', '1', current_timestamp(), '0'),
+    ('4', '4', '4', '2', '2', current_timestamp(), '0'),
+    ('5', '5', '5', '1', '1', current_timestamp(), '0'),
+    ('6', '4', '6', '2', '2', current_timestamp(), '0');
