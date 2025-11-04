2057dfe28f9a5c5d2f74bccad1a33ae3ddffb66c
LordBufu <bufu_39@hotmail.com>
2025-11-04 14:22:41 +0100

    Adding more status and loaner routers, for the last few popins.

--- ext/config/routes.php
+++ ext/config/routes.php
@@ -1,23 +1,26 @@
 <?php
 /*  App User-Managment routes */
-$router->get(       '/',                'ViewController@landing');
-$router->get(       '/login',           'AuthController@showform');
-$router->post(      '/login',           'AuthController@authenticate');
-$router->post(      '/logout',          'AuthController@logout');
-$router->patch(     '/resetPassword',   'AuthController@resetPassword');
+$router->get(       '/',                        'ViewController@landing');
+$router->get(       '/login',                   'AuthController@showform');
+$router->post(      '/login',                   'AuthController@authenticate');
+$router->post(      '/logout',                  'AuthController@logout');
+$router->patch(     '/resetPassword',           'AuthController@resetPassword');
 
 /*  App main view routes */
-$router->get(       '/home',            'ViewController@home');
+$router->get(       '/home',                    'ViewController@home');
 
 /*  App book related routes */
-$router->get(       '/bookdata',        'BookController@bookdata');
-$router->post(      '/addBook',         'BookController@add');
-$router->patch(     '/editBook',        'BookController@edit');
-$router->delete(    '/delBook',         'BookController@delete');
+$router->get(       '/bookdata',                'BookController@bookdata');
+$router->post(      '/addBook',                 'BookController@add');
+$router->patch(     '/editBook',                'BookController@edit');
+$router->delete(    '/delBook',                 'BookController@delete');
 
 /*  App status related routes */
-$router->patch(     '/changeStatus',    'StatusController@changeStatus');
-$router->get(       '/requestStatus',   'StatusController@requestStatus');
+$router->patch(     '/setStatusPeriod',         'StatusController@setStatusPeriod');
+$router->patch(     '/changeStatus',            'StatusController@changeStatus');
+$router->get(       '/requestStatus',           'StatusController@requestStatus');
+$router->get(       '/requestBookStatus',       'StatusController@requestBookStatus');
 
 /*  App loaner related routes */
-$router->get(       '/requestLoaners',  'LoanerController@requestLoaners');
+$router->get(       '/requestLoaners',          'LoanerController@requestLoaners');
+$router->get(       '/requestLoanerForBook',    'LoanerController@requestLoanerForBook');
