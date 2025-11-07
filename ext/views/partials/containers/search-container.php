b1bc7889cf22b52fa11076e5536af1c7f6f5f06b
LordBufu <bufu_39@hotmail.com>
2025-11-07 11:19:51 +0100

    Removing redundant elements, and adding a clear icon when there is search text.

--- ext/views/partials/containers/search-container.php
+++ ext/views/partials/containers/search-container.php
@@ -1,18 +1,17 @@
-<div class="aletho-dropdown-body collapse" id="customSearchDropdown" data-toggle-target="#id">
-    <div class="aletho-dropdown-content" id="search-cont">
-        <label class="aletho-labels search-label" for="search-options">Boek zoeken:</label>
-        <form class="form-row" id="search-form" novalidate>
-            <select class="aletho-inputs" name="search-method" id="search-options">
-                <option value="title">Titel</option>
-                <option value="writer">Schrijver</option>
-                <option value="genre">Genre</option>
-            </select>
-            <input class="aletho-inputs"
+<form class="search-form" id="search-form" novalidate>
+    <select class="aletho-inputs" name="search-method" id="search-options">
+        <option value="title">Titel</option>
+        <option value="writer">Schrijver</option>
+        <option value="genre">Genre</option>
+    </select>
+
+    <div class="aletho-input-wrapper">
+        <input  class="aletho-inputs"
                 id="search-inp"
                 type="text"
                 name="searchInp"
                 placeholder="Zoek op titel ..."
                 required>
-        </form>
+        <span id="clear-search-icon" class="clear-search-icon">×</span>
     </div>
-</div>
+</form>
