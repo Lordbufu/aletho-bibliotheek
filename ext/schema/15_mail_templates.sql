acbcc2c16e650bb7e6a20a958d8e4135cdacc408
LordBufu <bufu_39@hotmail.com>
2025-11-07 15:28:17 +0100

    Reworking the templated database table, in perperation for the notification system.

--- ext/schema/15_mail_templates.sql
+++ ext/schema/15_mail_templates.sql
@@ -1,15 +1,13 @@
 -- Database Schema for Mail Template Management
 CREATE TABLE `mail_templates` (
     `id` INT(11) UNSIGNED AUTO_INCREMENT,
-    `mail_type` VARCHAR(50) NOT NULL,
+    `event_type` VARCHAR(50) NOT NULL,
+    `subject` VARCHAR(255) NOT NULL,
     `from_mail` VARCHAR(50) NOT NULL,
     `from_name` VARCHAR(50) NOT NULL,
-    `header_string` TEXT NOT NULL,
-    `body_text_1` TEXT NOT NULL,
-    `body_text_2` TEXT DEFAULT NULL,
-    `action_intro` TEXT NOT NULL,
-    `footer_outro` TEXT NOT NULL,
-    `footer_notes` TEXT DEFAULT NULL,
+    `body_html` TEXT NOT NULL,
+    `body_text` TEXT DEFAULT NULL,
+    `active` TINYINT(1) DEFAULT 1,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
