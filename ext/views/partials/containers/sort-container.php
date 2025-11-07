f79db4395f988ae64d1f6b8aeef9f04e3c1197e5
LordBufu <bufu_39@hotmail.com>
2025-11-07 11:20:07 +0100

    Removing the old container

--- ext/views/partials/containers/sort-container.php
+++ ext/views/partials/containers/sort-container.php
@@ -1,13 +1,11 @@
 <div class="sort-container">
-    <div class="sort-group">
-        <label for="sort-options">Sorteren op:</label>
-        <select id="sort-options" class="aletho-select">
-            <option value="title-asc">Titel oplopend</option>
-            <option value="title-desc">Titel aflopend</option>
-            <option value="writer-asc">Schrijver oplopend</option>
-            <option value="writer-desc">Schrijver aflopend</option>
-            <option value="genre-asc">Genre oplopend</option>
-            <option value="genre-desc">Genre aflopend</option>
-        </select>
-    </div>
+    <label for="sort-options">Sorteren</label>
+    <select id="sort-options" class="aletho-select">
+        <option value="title-asc">Titel oplopend</option>
+        <option value="title-desc">Titel aflopend</option>
+        <option value="writer-asc">Schrijver oplopend</option>
+        <option value="writer-desc">Schrijver aflopend</option>
+        <option value="genre-asc">Genre oplopend</option>
+        <option value="genre-desc">Genre aflopend</option>
+    </select>
 </div>
