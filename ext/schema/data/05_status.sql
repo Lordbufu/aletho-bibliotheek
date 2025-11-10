5255915ad47bed6d890d380cdb7157eb32f6f280
LordBufu <bufu_39@hotmail.com>
2025-11-10 15:42:03 +0100

    Improving status and mail template default database datasets.

--- ext/schema/data/05_status.sql
+++ ext/schema/data/05_status.sql
@@ -1,7 +1,10 @@
--- Insert default status data (not correctly filled in yet)
-INSERT INTO `status` (`id`, `type`, `periode_length`, `reminder_day`, `overdue_day`, `active`) VALUES
-    (NULL, 'Aanwezig', NULL, NULL, NULL, '1'),
-    (NULL, 'Afwezig', '14', '3', '1', '1'),
-    (NULL, 'Transport', '7', '1', NULL, '1'),
-    (NULL, 'Gereserveerd', NULL, '3', '1', '1'),
-    (NULL, 'Overdatum', '1', '1', '1', '1');
+-- Insert default status data
+INSERT INTO `status`
+    (`type`, `periode_length`, `reminder_day`, `overdue_day`, `active`)
+VALUES
+    ('Aanwezig', NULL, NULL, NULL, '1'),
+    ('Afwezig', '14', '3', '1', '1'),
+    ('Transport', '7', '1', '1', '1'),
+    ('Ligt Klaar', '7', '1', '1', '1'),
+    ('Gereserveerd', '7', '3', '1', '1'),
+    ('Overdatum', NULL, NULL, NULL, '1');
--- ext/schema/data/14_mail_templates_data.sql
+++ ext/schema/data/14_mail_templates_data.sql
@@ -23,7 +23,55 @@ VALUES
         <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Bedankt voor het lenen van :book_name. Het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
-    1),
+    1
+),
+(   'pickup_ready_confirm',
+    'Het boek: :book_naam is klaar voor ophalen',
+    ':from_mail',
+    ':from_name',
+    '<body>
+        <div class="header-content">
+            <h2 class="header-text">Hallo :user_name.</h2>
+        </div>
+        <div class="body-content">
+            <p class="body-text-1">
+                Bedankt voor het lenen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
+                Het boek is aangekomen bij <strong>:office</strong>, en kan worden opgehaald.
+            </p>
+            <p class="body-text-2">
+                De leenperiode gaat pas van start, als u het boek heeft opgehaald.
+            </p>
+        </div>
+    </body>
+    <footer class="footer-content">
+        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+    </footer>',
+    'Hallo :user_name. Bedankt voor het lenen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.Het boek is aangekomen bij <strong>:office</strong>, en kan worden opgehaald. De leenperiode gaat pas van start, als u het boek heeft opgehaald. Mvg & Bvd, De Aletho Bibliotheek.',
+    1
+),
+(   'pickup_confirm',
+    'Lening eind datum voor: :book_name',
+    ':from_mail',
+    ':from_name',
+    '<body>
+        <div class="header-content">
+            <h2 class="header-text">Hallo :user_name.</h2>
+        </div>
+        <div class="body-content">
+            <p class="body-text-1">
+                Bedankt voor het ophalen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
+            </p>
+            <p class="body-text-2">
+                De leenperiode is gestart, en het einde van de huidige leenperiode is: <strong>:due_date</strong>.
+            </p>
+        </div>
+    </body>
+    <footer class="footer-content">
+        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+    </footer>',
+    'Hallo :user_name. Bedankt voor het ophalen van :book_name. De leenperiode is gestart, en het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
+    1
+),
 (   'return_reminder',
     'Verloop reminder voor: :book_name',
     ':from_mail',
@@ -47,7 +95,8 @@ VALUES
         <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. De leen periode van: :book_name, is bijna verlopen. Vergeet niet om het boek op tijd terug te brengen naar de bibliotheek. Het einde van de huidige leenperiode is: :due_date. :action_block Mvg & Bvd, De Aletho Bibliotheek.',
-    1),
+    1
+),
 (   'transport_request',
     'Transport verzoek voor: :book_name',
     ':from_mail',
@@ -67,4 +116,50 @@ VALUES
         <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Er is een transport verzoek voor: :book_name. Zou je het boek mee willen nemen naar: :office. Mvg & Bvd, De Aletho Bibliotheek.',
-    1);
+    1
+),
+(   'reserv_confirm',
+    'Reservering bevestiging voor: :book_name',
+    ':from_mail',
+    ':from_name',
+    '<body>
+        <div class="header-content">
+            <h2 class="header-text">Hallo :user_name.</h2>
+        </div>
+        <div class="body-content">
+            <p class="body-text-1">
+                Bedankt voor het Reserveren van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
+                Als de leenperiode bijna is verstreken, krijg je nog een herrinering van ons.
+            </p>
+            <p class="body-text-2">
+                Het einde van de huidige leenperiode is: <strong>:due_date</strong>.
+            </p>
+        </div>
+    </body>
+    <footer class="footer-content">
+        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+    </footer>',
+    'Hallo :user_name. Bedankt voor het reserveren van :book_name. Het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
+    1
+),
+(   'overdue_notice',
+    'Verstreken lening voor: :book_name',
+    ':from_mail',
+    ':from_name',
+    '<body>
+        <div class="header-content">
+            <h2 class="header-text">Hallo :user_name.</h2>
+        </div>
+        <div class="body-content">
+            <p class="body-text-1">
+                Het is ons opgevallen dat de lening van <strong>:book_name</strong> is verstreken.
+                Breng het boek zo snel mogelijk terug naar de bibliotheek, zodat andere het boek ook kunnen lenen.
+            </p>
+        </div>
+    </body>
+    <footer class="footer-content">
+        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+    </footer>',
+    'Hallo :user_name.Het is ons opgevallen dat de lening van <strong>:book_name</strong> is verstreken.Breng het boek zo snel mogelijk terug naar de bibliotheek, zodat andere het boek ook kunnen lenen. Mvg & Bvd, De Aletho Bibliotheek.',
+    1
+);
