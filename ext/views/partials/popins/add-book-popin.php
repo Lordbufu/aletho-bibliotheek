d684c5669e21990656050a1cee12bf5fb0ce8f28
LordBufu <bufu_39@hotmail.com>
2025-11-13 14:29:49 +0100

    z-index change to fix hidden suggestions and related notifications.

--- ext/views/partials/popins/add-book-popin.php
+++ ext/views/partials/popins/add-book-popin.php
@@ -1,15 +1,12 @@
 <div id="add-book-popin" class="modal clear-on-close" tabindex="-1" style="display:none;">
     <div class="modal-dialog modal-dialog-centered" style="margin:auto;">
         <div class="modal-content aletho-modal-content">
-
             <div class="aletho-header modal-header pt-2 pb-2">
                 <h5 class="modal-title w-100">Boek Toevoegen</h5>
                 <button type="button" class="btn-close btn-close-white" id="close-add-book-popin"></button>
             </div>
-
             <div class="aletho-modal-body">
                 <form id="add-book-form" method="post" action="/addBook">
-                    
                     <div class="input-group input-group-sm">
                         <label for="book-name" class="aletho-labels extra-popin-style">Boeknaam</label>
                         <input  type="text"
@@ -21,7 +18,6 @@
                             <div class="aletho-alert-inline aletho-border"><?= htmlspecialchars($popErrors['book_name']) ?></div>
                         <?php endif; ?>
                     </div>
-
                     <div class="input-group input-group-sm">
                         <label for="writer" class="aletho-labels extra-popin-style" data-context="popin">Schrijver</label>
                         <div class="add-writer-tags-container" data-context="popin"></div>
@@ -35,7 +31,6 @@
                             <div class="aletho-alert-inline aletho-border"><?= htmlspecialchars($popErrors['book_writers']) ?></div>
                         <?php endif; ?>
                     </div>
-
                     <div class="input-group input-group-sm">
                         <label for="genre" class="aletho-labels extra-popin-style" data-context="popin">Genre</label>
                         <div class="add-genre-tags-container" data-context="popin"></div>
@@ -49,7 +44,6 @@
                             <div class="aletho-alert-inline aletho-border"><?= htmlspecialchars($popErrors['book_genres']) ?></div>
                         <?php endif; ?>
                     </div>
-
                     <div class="input-group input-group-sm">
                         <label for="office" class="aletho-labels extra-popin-style" data-context="popin">Locatie</label>
                         <div class="add-office-tags-container" data-context="popin"></div>
@@ -63,7 +57,6 @@
                             <div class="aletho-alert-inline aletho-border"><?= htmlspecialchars($popErrors['book_offices']) ?></div>
                         <?php endif; ?>
                     </div>
-                    
                     <button type="submit" class="aletho-buttons extra-input-style">Opslaan</button>
                 </form>
             </div>
