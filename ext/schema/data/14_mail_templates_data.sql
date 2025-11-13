4322cb03735980cd2ede93c0a5d33b48e1413737
LordBufu <bufu_39@hotmail.com>
2025-11-13 14:25:44 +0100

    Adding the correct text class tags to the data mail templates.

--- ext/schema/data/14_mail_templates_data.sql
+++ ext/schema/data/14_mail_templates_data.sql
@@ -7,20 +7,20 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Bedankt voor het lenen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
                 Als de leenperiode bijna is verstreken, krijg je nog een herrinering van ons.
             </p>
-            <p class="body-text-2">
+            <p class="tussenkoppen-tekst">
                 Het einde van de huidige leenperiode is: <strong>:due_date</strong>.
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Bedankt voor het lenen van :book_name. Het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -31,20 +31,20 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-teks">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Bedankt voor het lenen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
                 Het boek is aangekomen bij <strong>:office</strong>, en kan worden opgehaald.
             </p>
-            <p class="body-text-2">
+            <p class="tussenkoppen-tekst">
                 De leenperiode gaat pas van start, als u het boek heeft opgehaald.
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Bedankt voor het lenen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.Het boek is aangekomen bij <strong>:office</strong>, en kan worden opgehaald. De leenperiode gaat pas van start, als u het boek heeft opgehaald. Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -55,19 +55,19 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Bedankt voor het ophalen van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
             </p>
-            <p class="body-text-2">
+            <p class="tussenkoppen-tekst">
                 De leenperiode is gestart, en het einde van de huidige leenperiode is: <strong>:due_date</strong>.
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Bedankt voor het ophalen van :book_name. De leenperiode is gestart, en het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -78,21 +78,21 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 De leen periode van: <strong>:book_name</strong>, is bijna verlopen.
                 Vergeet niet om het boek op tijd terug te brengen naar de bibliotheek.
             </p>
-            <p class="body-text-2">
+            <p class="tussenkoppen-tekst">
                 Het einde van de huidige leenperiode is: <strong>:due_date</strong>.
             </p>
         </div>
         :action_block
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. De leen periode van: :book_name, is bijna verlopen. Vergeet niet om het boek op tijd terug te brengen naar de bibliotheek. Het einde van de huidige leenperiode is: :due_date. :action_block Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -103,17 +103,17 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Er is een transport verzoek voor: <strong>:book_name</strong>.
                 Zou je het boek mee willen nemen naar: <strong>:office</strong>
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Er is een transport verzoek voor: :book_name. Zou je het boek mee willen nemen naar: :office. Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -124,20 +124,20 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Bedankt voor het Reserveren van <strong>:book_name</strong>, we hopen dat het boek je gaat bevallen.
                 Als de leenperiode bijna is verstreken, krijg je nog een herrinering van ons.
             </p>
-            <p class="body-text-2">
+            <p class="tussenkoppen-tekst">
                 Het einde van de huidige leenperiode is: <strong>:due_date</strong>.
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name. Bedankt voor het reserveren van :book_name. Het einde van de huidige leenperiode is: :due_date. Mvg & Bvd, De Aletho Bibliotheek.',
     1
@@ -148,17 +148,17 @@ VALUES
     ':from_name',
     '<body>
         <div class="header-content">
-            <h2 class="header-text">Hallo :user_name.</h2>
+            <h2 class="titel-tekst">Hallo :user_name.</h2>
         </div>
         <div class="body-content">
-            <p class="body-text-1">
+            <p class="tussenkoppen-tekst">
                 Het is ons opgevallen dat de lening van <strong>:book_name</strong> is verstreken.
                 Breng het boek zo snel mogelijk terug naar de bibliotheek, zodat andere het boek ook kunnen lenen.
             </p>
         </div>
     </body>
     <footer class="footer-content">
-        <p class="footer-goodbye">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
+        <p class="overige-tekst">Mvg & Bvd, <br> De Aletho Bibliotheek.</p><hr>
     </footer>',
     'Hallo :user_name.Het is ons opgevallen dat de lening van <strong>:book_name</strong> is verstreken.Breng het boek zo snel mogelijk terug naar de bibliotheek, zodat andere het boek ook kunnen lenen. Mvg & Bvd, De Aletho Bibliotheek.',
     1
