b8347239b7f507d11285ee5af80ec52dabf82e62
LordBufu <bufu_39@hotmail.com>
2025-11-14 14:09:30 +0100

    Removing getenv(), that does work for my structure. Also adding a bit of fallback for debug reasons.

--- ext/config/app.php
+++ ext/config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'env'   => getenv('APP_ENV'),
-    'debug' => filer_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN),
+    'env'   => $_ENV['APP_ENV']   ?? $_SERVER['APP_ENV']   ?? 'production',
+    'debug' => filter_var($_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? 'false', FILTER_VALIDATE_BOOLEAN),
     'url'   => 'https://localhost',
 ];
--- ext/config/mail.php
+++ ext/config/mail.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'host'          => getenv('MAIL_HOST'),
-    'port'          => getenv('MAIL_PORT'),
-    'username'      => getenv('MAIL_USER'),
-    'password'      => getenv('MAIL_PASS'),
+    'host'          => $_ENV['MAIL_HOST'] ?? $_SERVER['MAIL_HOST'] ?? 'localhost',
+    'port'          => $_ENV['MAIL_PORT'] ?? $_SERVER['MAIL_PORT'] ?? '587',
+    'username'      => $_ENV['MAIL_USER'] ?? $_SERVER['MAIL_USER'] ?? 'user',
+    'password'      => $_ENV['MAIL_PASS'] ?? $_SERVER['MAIL_PASS'] ?? 'pass',
     'from_email'    => 'noreply.bibliotheek@aletho.nl',
     'from_name'     => 'Aletho Bibliotheek',
 ];
