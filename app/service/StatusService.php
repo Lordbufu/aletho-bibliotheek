ef5c48a766aa89d9b13206806c5cb8f62def6c5f
LordBufu <bufu_39@hotmail.com>
2025-12-01 15:54:13 +0100

    re-factor to only load 1 specific library not all.

--- app/service/StatusService.php
+++ app/service/StatusService.php
@@ -3,41 +3,45 @@ namespace App\Service;
 
 use App\App;
 
+/** Status Service file, basicaly a facade for the SatusRepo. */
 class StatusService {
-    protected \App\Libraries    $libs;
-    protected \App\Database     $db;
+    protected \App\Libs\StatusRepo  $statuses;
+    protected \App\Database         $db;
 
     public function __construct() {
         try {
-            $this->libs = App::getLibraries();
-            $this->db   = App::getService('database');
+            $this->statuses = App::getLibrary('status');
+            $this->db       = App::getService('database');
         } catch (\Throwable $t) {
             throw $t;
         }
     }
 
-    /* */
     public function getAllStatuses($tag = null): array {
-        return $this->status->getAllStatuses($tag);
+        return $this->statuses->getAllStatuses($tag);
     }
 
-    /* */
     public function getBookStatus(int $bookId): ?string {
-        return $this->status->getBookStatus($bookId);
+        return $this->statuses->getBookStatus($bookId);
+    }
+    
+    public function getStatusById(int $statusId): ?array {
+        return $this->statuses->getStatusById($statusId);
     }
 
-    /* */
     public function getBookDueDate(int $bookId): ?string {
-        return $this->status->getBookDueDate($bookId);
+        return $this->statuses->getBookDueDate($bookId);
     }
 
-    /*  Update status period settings */
     public function updateStatusPeriod(int $statusId, ?int $periode_length, ?int $reminder_day, ?int $overdue_day): bool {
-        return $this->status->updateStatusPeriod($statusId, $periode_length, $reminder_day, $overdue_day);
+        return $this->statuses->updateStatusPeriod($statusId, $periode_length, $reminder_day, $overdue_day);
     }
 
-    /* */
-    public function setBookStatus(int $bookId, int $statusId, ?int $metaId = null, ?int $loanerId = null, ?int $locationId = null, bool $sendMail = false): bool {
-        return $this->status->setBookStatus($bookId, $statusId, $metaId, $loanerId, $locationId, $sendMail);
+    public function setBookStatus(int $bookId, int $statusId): bool {
+        return $this->statuses->setBookStatus($bookId, $statusId);
+    }
+
+    public function updateBookStatusContext(int $bookStatusId, array $actionContext = [], ?bool $finished = null): bool {
+        return $this->statuses->updateBookStatusContext($bookStatusId, $actionContext, $finished);
     }
 }
