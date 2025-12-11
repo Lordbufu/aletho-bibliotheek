f113def030d3e1d2bb28240837f481d972f6b6de
LordBufu <bufu_39@hotmail.com>
2025-12-11 15:52:56 +0100

    updating the status service

--- app/service/StatusService.php
+++ app/service/StatusService.php
@@ -7,22 +7,38 @@ use App\App;
 class StatusService {
     protected \App\Libs\StatusRepo  $statuses;
     protected \App\Database         $db;
+    protected array                 $eventStatusMap;
 
-    public function __construct() {
+    public function __construct(array $config) {
         try {
-            $this->statuses = App::getLibrary('status');
-            $this->db       = App::getService('database');
+            $this->statuses         = App::getLibrary('status');
+            $this->db               = App::getService('database');
+            $this->eventStatusMap   = $config;
         } catch (\Throwable $t) {
             throw $t;
         }
     }
 
+    /** Local API: Request `eventStatusMap` from the config. */
+    public function getEventStatusMap() {
+        return $this->eventStatusMap;
+    }
+
+    /** External API's, as detailed in the `StatusRepo` */
     public function getAllStatuses($tag = null): array {
         return $this->statuses->getAllStatuses($tag);
     }
 
-    public function getBookStatus(int $bookId): ?string {
-        return $this->statuses->getBookStatus($bookId);
+    public function getStatusLinks(int $bookStatusId, int $statusId): array {
+        return $this->statuses->getStatusLinks($bookStatusId, $statusId);
+    }
+
+    public function disableBookStatus(int $bookId): bool {
+        return $this->statuses->disableBookStatus($bookId);
+    }
+
+    public function getBookStatus(int $bookId, string $flag = "type"): ?string {
+        return $this->statuses->getBookStatus($bookId, $flag);
     }
     
     public function getStatusById(int $statusId): ?array {
@@ -37,8 +53,24 @@ class StatusService {
         return $this->statuses->updateStatusPeriod($statusId, $periode_length, $reminder_day, $overdue_day);
     }
 
-    public function setBookStatus(int $bookId, int $statusId): bool {
+    public function updateStatusLinks(int $bookStatusId, int $notifId): bool {
+        return $this->statuses->updateStatusLinks($bookStatusId, $notifId);
+    }
+
+    public function setBookStatus(int $bookId, int $statusId): ?int {
         return $this->statuses->setBookStatus($bookId, $statusId);
+    }
+    
+    public function updateBookStatus(int $bookId, int $requestedStatusId, bool $transport): array {
+        return $this->statuses->updateBookStatus($bookId, $requestedStatusId, $transport);
+    }
+
+    public function linkEventIfNeeded(array $statusResult, int $requestedStatusId, int $oldStatus, string $trigger, array $requestStatus): ?int {
+        return $this->statuses->linkEventIfNeeded($statusResult, $requestedStatusId, $oldStatus, $trigger, $requestStatus);
+    }
+
+    public function getNotificationsForStatus(int $statusId): array {
+        return $this->statuses->getNotificationsForStatus($statusId);
     }
 
     public function updateBookStatusContext(int $bookStatusId, array $actionContext = [], ?bool $finished = null): bool {
