354b0e139b02bd4988be9c3515e56c310a6cd9dc
LordBufu <bufu_39@hotmail.com>
2025-12-22 15:47:13 +0100

    Adding more notification events

--- ext/schema/data/07_notifications.sql
+++ ext/schema/data/07_notifications.sql
@@ -5,5 +5,8 @@ INSERT INTO `notifications` (`type`, `active`, `template_id`) VALUES
     ('pickup_confirm', '1', '3'),
     ('return_reminder', '1', '4'),
     ('transport_request', '1', '5'),
+    ('transp_req_auto', '1', '5'),
     ('reserv_confirm', '1', '6'),
-    ('overdue_reminder', '1', '7');
+    ('reserv_confirm_auto', '1', '6'),
+    ('overdue_reminder_user', '1', '7')
+    ('overdue_reminder_admin', '1', '7');
