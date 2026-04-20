dc7110c309c63507cc900c5c671fca3804730a93
LordBufu <bufu_39@hotmail.com>
2026-04-20 13:54:16 +0200

    Introduction a hardcoded notification type map and resolver, this might become configurable later.

---
+++ app/engine/map/NotificationMap.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Engine\Map;
+
+final class NotificationMap {
+    /** Global: Pre-defined map for status_type -> notification_type */
+    public static array $map = [
+        'Afwezig' => [
+            'Aanwezig'   => 'loan_confirm',
+            'Ligt Klaar' => 'pickup_confirm',
+        ],
+
+        'Transport'    => 'transport_request',
+        'Ligt Klaar'   => 'pickup_ready_confirm',
+        'Gereserveerd' => 'reserv_confirm',
+        'Return'       => 'return_reminder',
+        'Overdatum'    => 'overdue_reminder_user'
+    ];
+
+    /** API: Resolve the correct notification tag */
+    public static function resolveNotificationType(string $to, string $from): ?string {
+        // Case 1: 2D map (Afwezig)
+        if (isset(self::$map[$to]) && is_array(self::$map[$to])) {
+            return self::$map[$to][$from] ?? null;
+        }
+
+        // Case 2: simple 1D map
+        return self::$map[$to] ?? null;
+    }
+}
