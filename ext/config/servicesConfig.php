52056250e9cb8a23caa1a1f82617eaa5809f1c9c
LordBufu <bufu_39@hotmail.com>
2026-06-11 15:50:52 +0200

    Cleaning up the service config, more will follow soonish.

--- ext/config/servicesConfig.php
+++ ext/config/servicesConfig.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    // Core App services, already cleaned and changed.
+    // Config based services
     'router'        => [
         'class'     => \App\Router::class,
         'config'    => BASE_PATH . '/ext/config/routes.php'
@@ -10,59 +10,28 @@ return [
         'class'     => \App\Database::class,
         'config'    => BASE_PATH . '/ext/config/databaseConfig.php'
     ],
-
-    // Authentication and User related services
-    'auth'          => [                                      // Changed/Cleaned service 
-        'class'     => \App\Services\AuthService::class
-    ],
-    'user'          => [                                      // Changed/Cleaned service
-        'class'     => \App\Services\UserService::class
-    ],
-
-    // Validation services
-    'form_val'      => [                                      // Changed/Cleaned service
-        'class'     => \App\Validation\FormValidator::class
-    ],
-
-    // View & Flow Specific Services
-    'books'         => [                                      // Changed/Cleaned service
-        'class'     => \App\Services\BookService::class
-    ],
-
-    'book_status'   => [                                      // Changed/Cleaned service
-        'class'     => \App\Services\BookStatusService::class 
-    ],
-
-    'offices'      => [                                       // Changed/Cleaned service
-        'class'     => \App\Services\OfficesService::class
-    ],    
-
-    'statuses'      => [                                      // Changed/Cleaned service
-        'class'     => \App\Services\StatusService::class
-    ],
-
-    'loan'      => [                                          // Changed/Cleaned service
-        'class'     => \App\Services\LoanService::class
-    ],
-
-    'loaner'      => [                                        // Changed/Cleaned service
-        'class'     => \App\Services\LoanerService::class
-    ],
-
-    // Re-view to complete the refactor process.
     'notifications' => [
         'class' => \App\Services\NotificationService::class,
         'config' => BASE_PATH . '/ext/config/notificationConfig.php'
     ],
+
+    // Non-config core services
+    'user'          => [ 'class' => \App\Services\UserService::class ],
+    'location'      => [ 'class' => \App\Services\LocationService::class ],
+    'book'          => [ 'class' => \App\Services\BookService::class ],
+    'writer'        => [ 'class' => \App\Services\WriterService::class],
+    'genre'         => [ 'class' => \App\Services\GenreService::class],
+    'status'        => [ 'class' => \App\Services\StatusService::class ],
+
+    // Non-config layered services
+    'auth'          => [ 'class' => \App\Services\AuthService::class ],
+    'form_val'      => [ 'class' => \App\Validation\FormValidator::class ],
+
     
-    'mail' => [
-        'class' => \App\Services\MailTemplateService::class,
-        'config' => BASE_PATH . '/ext/config/mailTemplateConfig.php'
-    ],
 
-
-    // Extra functionality
-    // 'logger' => [
-    //     'class'  => \App\Services\LoggerService::class
-    // ],
+    'book_status'   => [ 'class' => \App\Services\BookStatusService::class ],
+    'loan'          => [ 'class' => \App\Services\LoanService::class ],
+    'loaner'        => [ 'class' => \App\Services\LoanerService::class ],
+    'mail'          => [ 'class' => \App\Services\MailTemplateService::class ],
+    // 'logger'        => [ 'class' => \App\Services\LoggerService::class ],       // Extra functionality for later
 ];
