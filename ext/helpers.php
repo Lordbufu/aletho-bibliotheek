ece5e1f1247a5966dc322f2d57adbf139d28c454
LordBufu <bufu_39@hotmail.com>
2026-06-11 15:47:41 +0200

    Adjusting the helper functions for the new workflows, and adding a new component function to present partial templates inline.

--- ext/helpers.php
+++ ext/helpers.php
@@ -1,14 +1,14 @@
 <?php
 /** Function to set viewpaths more cleanly. */
 if (!function_exists('viewPath')) {
-    function viewPath($string) {
-        return  __DIR__ . "/views/" . $string;
+    function viewPath(string $path) {
+        return  __DIR__ . "/views/" . $path;
     }
 }
 
 /** Dump and Die data on screen for debug reasons. */
 if (!function_exists('dd')) {
-    function dd($data) {
+    function dd(mixed $data) {
         echo '<pre style="background:#222;color:#0f0;padding:0.625rem;font-size:0.875rem;line-height:1.4;">';
         if (is_array($data) || is_object($data)) {
             print_r($data);
@@ -34,7 +34,7 @@ if(!function_exists('calculateDueDate')) {
  *      @param string|array $message    -> Message string or array (for form data)
  */
 if (!function_exists('setFlash')) {
-    function setFlash(string $bucket, $type, $message): void {
+    function setFlash(string $bucket, mixed $type, mixed $message): void {
         $buckets = [
             'global'    => '_flashGlobal',
             'inline'    => '_flashInline',
@@ -56,30 +56,43 @@ if (!function_exists('setFlash')) {
     }
 }
 
+// Re-factored helpers for inline premission checks
 /** Check if user is a guest */
 if(!function_exists('checkGuestRole')) {
-    function checkGuestRole() {
-        return ($_SESSION['user']['role'] ?? null) === 'Guest';
+    function checkGuestRole(string $role = 'guest') {
+        $flags = $_SESSION['user']['permission'] ?? [];
+        return in_array($role, $flags, true);
     }
 }
 
 /** Check if user is a global admin (currently used to seperate global and office admins) */
 if (!function_exists('isGlobalAdmin')) {
-    function isGlobalAdmin(): bool {
-        return ($_SESSION['user']['role'] ?? null) === 'global_admin';
+    function isGlobalAdmin(string $role = 'global_admin'): bool {
+        $flags = $_SESSION['user']['permission'] ?? [];
+        return in_array($role, $flags, true);
     }
 }
 
 /** Check if user is a office admin (currently unused) */
 if (!function_exists('isOfficeAdmin')) {
-    function isOfficeAdmin(): bool {
-        return ($_SESSION['user']['role'] ?? null) === 'office_admin';
+    function isOfficeAdmin(string $role = 'office_admin'): bool {
+        $flags = $_SESSION['user']['permission'] ?? [];
+        return in_array($role, $flags, true);
     }
 }
 
 /** Check if user can edit (is either a global or office admin) */
 if (!function_exists('canEdit')) {
-    function canEdit(): bool {
-        return in_array($_SESSION['user']['role'], ['office_admin', 'global_admin']);
+    function canEdit(array $roles = ['office_admin', 'global_admin']): bool {
+        $flags = $_SESSION['user']['permission'] ?? [];
+        return array_intersect($roles, $flags) !== [];
+    }
+}
+
+// View testing code
+if (!function_exists('component')) {
+    function component(string $name, array $data = []) {
+        extract($data);
+        require viewPath("partials/components/{$name}.php");
     }
 }
