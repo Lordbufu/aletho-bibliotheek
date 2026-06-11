e1e3e3ed4d0d3afbef29d6d872810a7244a6f441
LordBufu <bufu_39@hotmail.com>
2026-06-11 15:43:48 +0200

    Minor parameter change for the view function, to satisfy syntax validators.

--- app/App.php
+++ app/App.php
@@ -105,7 +105,7 @@ class App {
     }
 
     /** API: Return JSON data for the frontend logic  */
-    public static function json($data, int $status = 200): void {
+    public static function json(mixed $data, int $status = 200): void {
         http_response_code($status);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
