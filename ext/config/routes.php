6beae84ccc10dc70949bcb5e4e66196721a90b4b
LordBufu <bufu_39@hotmail.com>
2026-06-11 15:51:31 +0200

    Cleaning up the routes and adding re-factor notes.

--- ext/config/routes.php
+++ ext/config/routes.php
@@ -1,16 +1,16 @@
 <?php
 return [
     /** App main view routes */
-    ['GET',     '/',                    'ViewController@landing'],
-    ['GET',     '/home',                'ViewController@home'],
+    ['GET',     '/',                    'ViewController@landing'],                  // re-factored
+    ['GET',     '/home',                'ViewController@home'],                     // re-factored
 
     /** App User-Managment routes */
-    ['POST',   '/login',                'AuthController@login'],
-    ['POST',   '/logout',               'AuthController@logout'],
-    ['PATCH',  '/resetPassword',        'AuthController@resetPassword'],
+    ['POST',   '/login',                'AuthController@login'],                    // re-factored
+    ['POST',   '/logout',               'AuthController@logout'],                   // re-factored
+    ['PATCH',  '/resetPassword',        'AuthController@resetPassword'],            // re-factored
 
     /** App book related routes */
-    ['GET',    '/bookData',             'BookController@bookData'],
+    ['GET',    '/bookData',             'BookController@bookData'],                 // re-factored
     ['POST',   '/addBook',              'BookController@addBook'],
     ['PATCH',  '/editBook',             'BookController@editBook'],
     ['DELETE', '/delBook',              'BookController@deleteBook'],
@@ -19,11 +19,12 @@ return [
     ['PATCH',  '/editStatusPeriod',     'StatusController@editStatusPeriod'],
     ['PATCH',  '/changeStatus',         'StatusController@changeStatus'],    
     
-
     // XHR requests for frontend scripts:
-    ['GET',    '/requestStatus',        'StatusController@requestStatus'],
-    ['GET',    '/requestBookStatus',    'StatusController@requestBookStatus'],
-    ['GET',    '/requestPopinStatus',   'StatusController@requestPopinStatus'],
-    ['GET',    '/requestLoanerForBook', 'LoanerController@requestLoanerForBook'],
-    ['GET',    '/requestLoaners',       'LoanerController@requestLoaners'],
+    ['GET',    '/requestStatus',        'StatusController@requestStatus'],          // re-factored
+    ['GET',    '/requestPopinStatus',   'StatusController@requestPopinStatus'],     // re-factored
+    ['GET',    '/requestLoanerForBook', 'LoanerController@requestLoanerForBook'],   // re-factored
+    ['GET',    '/requestLoaners',       'LoanerController@requestLoaners'],         // re-factored
+
+    // Re-factor status: Potentially redundant now
+    // ['GET',    '/requestBookStatus',    'StatusController@requestBookStatus'],
 ];
