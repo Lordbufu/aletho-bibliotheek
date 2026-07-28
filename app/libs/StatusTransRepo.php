368ba775bd99c3c88ec3d52dd516215d1da436bd
LordBufu <bufu_39@hotmail.com>
2026-07-28 15:45:58 +0200

    Initial draft of the status transition repo.

---
+++ app/libs/StatusTransRepo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Libs;
+
+use App\Libs\Context\StatusTransitionContext;
+
+final class StatusTransRepo {
+    private \App\Database $db;
+
+    public function __construct() {
+        $this->db = \App\App::getService('database');
+    }
+
+    public function getTransitionByIds(int $from_status_id, int $to_status_id): StatusTransitionContext {
+        $row = $this->db->query()->fetchOne(
+            "SELECT * FROM status_transition WHERE from_status_id = :fromStId AND to_status_id = :toStId",
+            [
+                "fromStId" => $from_status_id,
+                "toStId"   => $to_status_id
+            ]
+        );
+
+        return new StatusTransitionContext($row);
+    }
+}
