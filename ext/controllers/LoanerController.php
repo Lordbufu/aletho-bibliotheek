888938616b4ac3f8b93ba6a09178055b7b28598d
LordBufu <bufu_39@hotmail.com>
2026-07-28 15:39:49 +0200

    Adding missing user verification.

--- ext/controllers/LoanerController.php
+++ ext/controllers/LoanerController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ext\Controllers;
 
+// TODO: Consider adjusting the view model for loaner data, and introducing a request email function, to reduce the risk of exposing all emails.
+// And also make sure to discuss this with the client, as this might mean that it takes a week or two longer then expected.
 // Re-factor status: tested and working
 class LoanerController {
     private \App\App                                    $app;
@@ -11,21 +13,25 @@ class LoanerController {
 
     /** XHR Request for: Filling in loaner suggestion lists */
     public function requestLoaners() {
-        $query = trim($_GET['query'] ?? '');
+        $this->app::getService('auth')->requireRole(['office_admin', 'global_admin']);
+
+        $query              = trim($_GET['query'] ?? '');
 
         if ($query === '' || mb_strlen($query) < 2) {
             return $this->app::json([]);
         }
         
-        $loaners = $this->app::getService('loaner')->searchLoaners($query);
+        $loaners            = $this->app::getService('loaner')->searchLoaners($query);
 
         return $this->app::json($loaners);
     }
 
     /** XHR Request for: Request loaner data for a specific book */
     public function requestLoanerForBook() {
-        $bookId = (int)$_GET['book_id'];
-        $loaner = $this->app::getService('loaner')->getLoanerForBook($bookId) ?? null;
+        $this->app::getService('auth')->requireRole(['office_admin', 'global_admin']);
+
+        $bookId             = (int)$_GET['book_id'];
+        $loaner             = $this->app::getService('loaner')->getLoanerForBook($bookId) ?? null;
 
         return $this->app::json($loaner);
     }
